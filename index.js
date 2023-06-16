f8b135f383131ea11be90a441cd06831fd52aebd
Francisco Santos <francisco.santos@midway.com.br>
2023-06-16 15:48:27 -0300

    update

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ const stream = require("./stream");
 const text = require("./text");
 const radio = require("radio-stream");
 
-var rstream = radio.createReadStream("https://hyades.shoutca.st:8043/stream");
-rstream.on("connect", function () {
-    console.log("Radio Stream connected!");
-});
+// var rstream = radio.createReadStream("https://hyades.shoutca.st:8043/stream");
+// rstream.on("connect", function () {
+//     console.log("Radio Stream connected!");
+// });
 
-rstream.on("metadata", function (title) {
-    title = title.replace("StreamTitle='", "").replace("';", "");
-    title = title.replace("Unknown - ", "").replace("- Unknown", "");
-    if (title == "support chillsky - support chillsky")
-        title = "Music provided by Chillsky";
+// rstream.on("metadata", function (title) {
+//     title = title.replace("StreamTitle='", "").replace("';", "");
+//     title = title.replace("Unknown - ", "").replace("- Unknown", "");
+//     if (title == "support chillsky - support chillsky")
+//         title = "Music provided by Chillsky";
 
-    text.ChangeText(title);
-});
+//     text.ChangeText(title);
+// });
 
 stream.Start();
-server.Listen(process.env.PORT || 80);
+// server.Listen(process.env.PORT || 80);
